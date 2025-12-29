a887e731a5cb1ee2d61ecfda61fc3c2e0020a90c
unknown <xiaquan666666@gmail.com>
2025-12-28 21:56:07 -0500

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="p-6 flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Address Book Application
-      </h1>
-    </main>
-  );
+  return <h1 className="text-4xl font-bold">Home</h1>;
 }
