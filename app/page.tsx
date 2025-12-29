79efedcece0d7b02e6bccb74f3a149e0e6a70909
unknown <xiaquan666666@gmail.com>
2025-12-28 23:21:52 -0500

    Add a button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Link from "next/link";
+'use client'
+
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <h1 className="text-4xl font-bold">Home</h1>;
+  return (
+    <main className="p-6 flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Address Book
+      </h1>
+      <Button onClick={handleClick}>
+      Add Contact
+    </Button>
+    </main>
+  )
+}
+
+function handleClick() {
+  alert('Button clicked!')
 }
